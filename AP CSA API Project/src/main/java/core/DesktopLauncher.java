c3bc859db2db91633bbeb8c363f741c11b07ae3b
ppranav0401 <113952286+ppranav0401@users.noreply.github.com>
2023-04-15 22:19:11 -0700

    Add files via upload

---
+++ AP CSA API Project/src/main/java/core/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package core;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+//import com.mygdx.game.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("core.Platformer");
+        config.setWindowedMode(960,640);
+        //config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        new Lwjgl3Application(new Platformer(), config);
+    }
+}
---
+++ AP CSA API Project/src/main/java/objects/player/Player.java
@@ -0,0 +1,53 @@
+package objects.player;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
+import static helper.Constants.PPM;
+
+public class Player extends GameEntity{
+
+    private int jumpCounter;
+    public Player(float width, float height, Body body) {
+        super(width, height, body);
+        this.speed = 10f;
+        this.jumpCounter=0;
+    }
+
+    @Override
+    public void update() {
+        x = body.getPosition().x * PPM;
+        y = body.getPosition().y * PPM;
+        checkUserInput();
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    private void checkUserInput(){
+        velX=0;
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            velX=1;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            velX=-1;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && jumpCounter<2){
+            float force = body.getMass()*8.5f;
+            body.applyLinearImpulse(new Vector2(0,force),body.getPosition(),true);
+            jumpCounter++;
+        }
+
+        //reset jumpCounter
+        if(body.getLinearVelocity().y==0){
+            jumpCounter=0;
+        }
+
+        body.setLinearVelocity(velX*speed,body.getLinearVelocity().y);
+    }
+}
