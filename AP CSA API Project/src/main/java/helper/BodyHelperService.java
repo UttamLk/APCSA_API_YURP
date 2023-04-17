12b3f07615fc8c73c2c8461d0fa756d48ce04963
ppranav0401 <113952286+ppranav0401@users.noreply.github.com>
2023-04-17 10:47:25 -0700

    Update BodyHelperService.java

--- AP CSA API Project/src/main/java/helper/BodyHelperService.java
+++ AP CSA API Project/src/main/java/helper/BodyHelperService.java
@@ -5,7 +5,13 @@ import com.badlogic.gdx.physics.box2d.*;
 import static helper.Constants.PPM;
 
 public class BodyHelperService {
-
+    /*
+    *Creates a body with the specified parameters
+    *@param x,y Positional coordinates
+    *@param width,height Width and Height of the Body
+    *@param isStatic If the body will be a StaticBody or DynamicBody
+    *@param world The world in which the body will be in.
+    */
     public static Body createBody(float x, float y, float width, float height, boolean isStatic, World world){
         BodyDef bodyDef = new BodyDef();
         //bodyDef.type = isStatic ? BodyDef.BodyType.StaticBody : BodyDef.BodyType.DynamicBody;
